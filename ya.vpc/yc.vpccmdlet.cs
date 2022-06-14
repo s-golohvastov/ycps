dec861ffa4accd7708108373134724a4657ab382
Svirid-Golohvastov <sv.golohvastov@yandex.ru>
2022-06-14 15:40:07 -0700

    list/get vpc

---
+++ ya.vpc/yc.vpccmdlet.cs
@@ -0,0 +1,89 @@
+﻿using System.Management.Automation;
+using System.Security.Policy;
+using Yandex.Cloud.Resourcemanager.V1;
+using Yandex.Cloud.Vpc.V1;
+using yc.basecmdlet;
+using yc.config;
+using static Yandex.Cloud.Vpc.V1.NetworkService;
+
+namespace ya.vpc
+{
+    [Cmdlet(VerbsCommon.Get, "YcVpc", DefaultParameterSetName = "FolderId")]
+    public class GetYcVpcCmdlet : YcBase<NetworkServiceClient>
+    {
+        [Parameter(Mandatory = true, ParameterSetName = "FolderId")]
+        [Parameter(Mandatory = true, ParameterSetName = "FolderIdNameFiler")]
+        [ValidateNotNullOrEmpty]
+        public string? FolderId;
+        
+        [Parameter(Mandatory = true, ParameterSetName = "FolderObj", ValueFromPipeline = true)]
+        [Parameter(Mandatory = true, ParameterSetName = "FolderObjNameFiler", ValueFromPipeline = true)]
+        [ValidateNotNullOrEmpty]
+        public Folder? FolderObj;
+
+        [Parameter(Mandatory = false, ParameterSetName = "FolderIdNameFiler")]
+        [Parameter(Mandatory = false, ParameterSetName = "FolderObjNameFiler")]
+        [Alias("Name")]
+        public string? NetworkName;
+
+
+        [Parameter(Mandatory = true, ParameterSetName = "NetworkId")]
+        [ValidateNotNullOrEmpty]
+        public string? NetworkId;
+
+
+        protected override void ProcessRecord()
+        {
+            switch (this.ParameterSetName)
+            {
+                case "FolderId":
+                    GetAllVpcInFolder(FolderId);
+                    break;
+                case "FolderObj":
+                    GetAllVpcInFolder(FolderObj.Id);
+                    break;
+                case "FolderIdNameFiler":
+                    FilterVpcInFolderByName(FolderId, NetworkName);
+                    break;
+                case "FolderObjNameFiler":
+                    FilterVpcInFolderByName(FolderObj.Id, NetworkName);
+                    break;
+                case "NetworkId":
+                    GetVpcById(NetworkId);
+                    break;
+            }
+            base.ProcessRecord();
+        }
+
+
+        private void GetAllVpcInFolder(string folderId)
+        {
+            var listRequest = new ListNetworksRequest
+            {
+                PageSize = int.Parse(YcConfig.Instance.Configuration["Settings:defaultPageSize"]), 
+                FolderId = folderId
+            };
+            var res = base.grpcClient.List(listRequest, base.headers);
+            WriteObject(res.Networks, true);
+        }
+
+        private void GetVpcById(string networkId)
+        {
+            var getRequest = new GetNetworkRequest { NetworkId = networkId };
+            var res = base.grpcClient.Get(getRequest, base.headers);
+            WriteObject(res);
+        }
+
+        private void FilterVpcInFolderByName(string folderId, string networkName)
+        {
+            var listRequest = new ListNetworksRequest
+            {
+                PageSize = int.Parse(YcConfig.Instance.Configuration["Settings:defaultPageSize"]), 
+                Filter = $"name = \"{ networkName }\"",
+                FolderId = folderId
+            };
+            var res = base.grpcClient.List(listRequest, base.headers);
+            WriteObject(res.Networks, true);
+        }
+    }
+}
---
+++ yc.tests/pester.tests/yc.vpc.tests.ps1
@@ -0,0 +1,38 @@
+﻿# to run the tests locally we need to run Invoke-Pester directly in the pester.tests folder
+# the module itself must be present in c:\temp\ycps
+# the following modules must be also installed
+# Install-Module Microsoft.PowerShell.SecretManagement, Microsoft.PowerShell.SecretStore
+# few secrets must be created in the local PowerShell.SecretStore:
+# YandexOAuthToken, YandexTestFolderId, YandexTestNetworkId
+
+$ModuleManifestName = 'ipmgmt.psd1'
+$ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
+$modulePath = "$PSScriptRoot\.."
+
+# Import-Module $modulePath -Verbose
+Import-Module c:\temp\ycps -Verbose
+Connect-YcAccount -OAuthToken (Get-Secret -Name YandexOAuthToken -AsPlainText)
+
+
+Describe 'Basic Get-YcVpc tests' {
+    It '1. No parameters - list VPC in a folder' {
+        $vpc = Get-YcVpc -FolderId (Get-Secret -Name YandexTestFolderId -AsPlainText)
+        $vpc | Should -Not -BeNullOrEmpty
+    }
+
+    It '2. Get a VPC by Id (folder id not needed)' {
+        $vpc = Get-YcVpc -NetworkId (Get-Secret -Name YandexTestNetworkId -AsPlainText)
+        $vpc | Should -Not -BeNullOrEmpty
+    }
+
+    It '3. Simple filtering' {
+        $vpc = Get-YcVpc -FolderId (Get-Secret -Name YandexTestFolderId -AsPlainText) -NetworkName "default"
+        $vpc | Should -Not -BeNullOrEmpty
+    }
+
+    It '4. Pipeline binding folder objects/list all VMs in a set of folders' {
+        $cloudId = Get-Secret -Name YandexTestCloudId -AsPlainText
+        $vpc = Get-YcCloud -CloudId $cloudId | Get-YcFolder | Get-YcVpc -NetworkName "default"
+        $vpc | Should -Not -BeNullOrEmpty
+    }
+}
