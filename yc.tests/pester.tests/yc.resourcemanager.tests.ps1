e54af1ddda3b0d580bedc047871c82cc46b052a9
Svirid-Golohvastov <sv.golohvastov@yandex.ru>
2022-06-13 21:35:26 -0700

    basic set of pester tests

---
+++ yc.tests/pester.tests/yc.resourcemanager.tests.ps1
@@ -0,0 +1,45 @@
+﻿# to run the tests locally we need to run Invoke-Pester directly in the pester.tests folder
+# the following modules must be also installed
+# Install-Module Microsoft.PowerShell.SecretManagement, Microsoft.PowerShell.SecretStore
+# few secrets must be created in the local PowerShell.SecretStore:
+# YandexOAuthToken, YandexTestCloudId, YandexTestFolderId
+
+$ModuleManifestName = 'ipmgmt.psd1'
+$ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
+$modulePath = "$PSScriptRoot\.."
+
+# Import-Module $modulePath -Verbose
+Import-Module c:\temp\ycps -Verbose
+Connect-YcAccount -OAuthToken (Get-Secret -Name YandexOAuthToken -AsPlainText)
+
+
+Describe 'Basic Get-YcCloud tests' {
+    It 'No parameters - list clouds' {
+        Get-YcCloud
+    }
+}
+
+Describe 'Basic Get-YcFolder tests' {
+    It 'No parameters - list folders of a cloud' {
+        $testCloudId = (Get-Secret -Name YandexTestCloudId -AsPlainText)
+        
+        Get-YcFolder -CloudId $testCloudId
+    }
+
+    It 'Pipeline binding - iterate through clouds' {
+        Get-YcCloud | Get-YcFolder
+    }
+
+    It 'Get single folder by id' {
+        $testFolderId = (Get-Secret -Name YandexTestFolderId -AsPlainText)
+        
+        Get-YcFolder -FolderId $testFolderId
+    }
+}
+
+
+Describe 'Basic Organization tests' {
+    It 'Get organization - no parameters' {
+        Get-YcOrganization
+    }
+}
