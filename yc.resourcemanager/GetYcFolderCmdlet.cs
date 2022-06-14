9c9f5cab23f88d75de3faaa7c143ae3e6dc74662
Svirid-Golohvastov <sv.golohvastov@yandex.ru>
2022-06-13 20:45:41 -0700

    fix get/list folders

--- yc.resourcemanager/GetYcFolderCmdlet.cs
+++ yc.resourcemanager/GetYcFolderCmdlet.cs
@@ -10,29 +10,60 @@ using static Yandex.Cloud.Resourcemanager.V1.FolderService;
 
 namespace yc.resmgr
 {
-    [Cmdlet(VerbsCommon.Get, "YcFolder")]
+    [Cmdlet(VerbsCommon.Get, "YcFolder", DefaultParameterSetName = "CloudId")]
     public class GetYcFolderCmdlet : YcBase<FolderServiceClient>
     {
-        [Parameter(Mandatory = false, ValueFromPipeline = true)]
+        [Parameter(Mandatory = false, ValueFromPipeline = true, ParameterSetName = "FolderId")]
+        [ValidateNotNullOrEmpty]
         public string? FolderId;
+
+        [Parameter(Mandatory = true, ValueFromPipeline = true, ParameterSetName = "CloudId")]
+        [ValidateNotNullOrEmpty]
+        public string? CloudId;
+
+        [Parameter(Mandatory = true, ValueFromPipeline = true, ParameterSetName = "Cloud")]
+        [ValidateNotNullOrEmpty]
+        public Cloud[] Cloud;
         protected override void ProcessRecord()
         {
-            if (string.IsNullOrEmpty(FolderId))
-            {
-                // list
-                var listFolderRequest = new ListFoldersRequest();
-                var res = base.grpcClient.List(listFolderRequest, base.headers);
-                WriteObject(res.Folders, true);
 
-            }
-            else
+            switch (this.ParameterSetName)
             {
-                // specific folder
-                var getFolderRequest = new GetFolderRequest { FolderId = FolderId };
-                var res = base.grpcClient.Get(getFolderRequest, base.headers);
-                WriteObject(res);
+                case "CloudId":
+                    ListFoldersByCloudId(CloudId);
+                    break;
+                case "FolderId":
+                    GetFolderByFolderId();
+                    break;
+                case "Cloud":
+                    GetFolderByCloudObj();
+                    break;
             }
 
+        }
+
+        private void GetFolderByCloudObj()
+        {
+            foreach (var c in Cloud)
+            {
+                ListFoldersByCloudId(c.Id);
+            }
+        }
+
+        private void GetFolderByFolderId()
+        {
+            // specific folder
+            var getFolderRequest = new GetFolderRequest { FolderId = FolderId };
+            var res = base.grpcClient.Get(getFolderRequest, base.headers);
+            WriteObject(res);
+        }
+
+        private void ListFoldersByCloudId(string id)
+        {
+            // list
+            var listFoldersRequest = new ListFoldersRequest { CloudId = id} ;
+            var res = base.grpcClient.List(listFoldersRequest, base.headers);
+            WriteObject(res.Folders, true);
         }
     }
 }
