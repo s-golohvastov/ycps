422694d385c30f0f9ce456f32245ddf577f15160
Svirid-Golohvastov <sv.golohvastov@yandex.ru>
2022-06-18 19:49:01 -0700

    get-yczones

---
+++ yc.compute/GetYcZones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+using Yandex.Cloud.Compute.V1;
+using static Yandex.Cloud.Compute.V1.ZoneService;
+using yc.basecmdlet;
+
+namespace yc.compute
+{
+    [Cmdlet(VerbsCommon.Get, "YcZone")]
+    public class GetYcZone : YcBase<ZoneServiceClient>
+    {
+
+        // TODO: create pester tests
+        protected override void ProcessRecord()
+        {
+            var zoneRequest = new ListZonesRequest();
+            var res = base.grpcClient.List(zoneRequest, base.headers);
+            WriteObject(res.Zones, true);
+        }
+    }
+}
